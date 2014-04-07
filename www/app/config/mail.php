593bc738f35a06396b6b702465b7754fb673564d
Kevin Mitchell <kevin@develpr.com>
2014-04-06 17:47:48 -0700

    Add default mail settings
    Need these for using Mail

--- www/app/config/mail.php
+++ www/app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'kevin@develpr.com', 'name' => 'Kevin Mitchell'),
 
 	/*
 	|--------------------------------------------------------------------------
